38f917538e9286b6966016a0b4182ebb1e0e9bfb
wYan <skyyen999@gmail.com>
2016-08-26 23:37:04 +0800

    add template design pattern

---
+++ client/bigtalk/template/TestPaperClient.java
@@ -0,0 +1,22 @@
+package bigtalk.template;
+
+/**
+ * 考試卷-客戶端
+ * 一開始TestPaper類別就已經實作出題的method QuestionA(), QuestionB(), QuestionC() <br>
+ * 也實作了印出整張考卷做答與題目的method display()<br>
+ * 因此真正發給學生的考卷TestPaperA, TestPaperB只要實現寫答案的method answerA(), answerB(), answerC()即可
+ * @author Yan
+ *
+ */
+public class TestPaperClient {
+	public static void main(String[] args) {
+		System.out.println("學生A的試卷");
+		TestPaper paperA = new TestPaperA();
+		paperA.display();
+
+		System.out.println();
+		System.out.println("學生B的試卷");
+		TestPaper paperB = new TestPaperB();
+		paperB.display();
+	}
+}
---
+++ src/bigtalk/template/TestPaperB.java
@@ -0,0 +1,19 @@
+package bigtalk.template;
+
+public class TestPaperB extends TestPaper{
+
+	@Override
+	protected String answerA() {
+		return "c";
+	}
+
+	@Override
+	protected String answerB() {
+		return "a";
+	}
+
+	@Override
+	protected String answerC() {
+		return "a";
+	}
+}
